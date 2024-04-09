3db8f28951d5c3cafd5c87b23fe6361b0ac2c9e1
서현재 <howlarang123@naver.com>
2024-04-09 16:56:53 +0900

    24.04.09 싸피에서 강의 수강

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
+
 import componentImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
-
+import TabButton from './components/TabBoutton.jsx';
+import { EXAMPLES } from './data.js'
 
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+
+  function handleSelect(selectedButton) {
+    // selectedButtoo => 'components', 'jsx','props','state'
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Time to get started!</h2>
@@ -26,6 +53,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+        </section>
+        {tabContent}
       </main>
     </div>
   );
